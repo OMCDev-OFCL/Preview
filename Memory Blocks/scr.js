1ea307dcf1db3b632f3f1fc25aba87dc1a86f7d8
OMCDev <omcdev.ofcl@gmail.com>
2020-11-12 23:53:24 +0530

    scr fix

--- Memory Blocks/scr.js
+++ Memory Blocks/scr.js
@@ -2,16 +2,16 @@ console.log("connected");
 var screenheight = window.innerHeight;
 var screenwidth = window.innerWidth;
 var container = document.getElementById("unityContainer").style;
-var footer = document.getElementById("footer").style;
-footer.display = 'none';
+var footer = document.getElementById("footer");
+
 if(screenwidth > screenheight)
 {
     container.height = screenheight+'px';
     container.width = (screenheight/2)+'px';
-    
+    footer.style.display = 'none';
 }
 else
 {
-    container.height = screenheight+'px';
+    container.height = (screenheight-footer.offsetHeight)+'px';
     container.width = screenwidth+'px';
 }
